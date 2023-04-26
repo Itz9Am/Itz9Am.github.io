b46c69fbb7186b2d15aa0be03122a60201649d88
Alvar Malmqvist <alvar.malmqvist@gmail.com>
2023-04-26 17:26:00 +0200

    key funktion

--- src/crypta/core.cljs
+++ src/crypta/core.cljs
@@ -2,10 +2,11 @@
   "Crypta" 
   (:require [clojure.string :as str]))
 
-;(def key-vector [-1 0 -3 2 1 -3 -1 1 5 -7 -4 1 -4 1 -4 1 -4 -8 6 -5 2 -6 -2 5 0 4 8 -7])
-(def key-vector [-1 0 -3 2 1 -3 -1 1 5 -7 -4 1 -4 -8 6 -5 2 -6 -2 5 0 4 8 -7])
+(def key-vector (atom []))
+(defn inv-key []
+  (map - @key-vector))
 
-(def inv-key (map - key-vector))
+(def hide-key-state (atom false))
 
 (def num->alpha (zipmap (range 29)
                         "abcdefghijklmnopqrstuvwxyzåäö"))
@@ -24,9 +25,9 @@
         (cons (code-letter letter key-digit)
               (code-msg (rest msg) (rest key)))))))
 
-(defn encode [input] (apply str (code-msg (str/lower-case input) (cycle key-vector))))
+(defn encode [input] (apply str (code-msg (str/lower-case input) (cycle @key-vector))))
 
-(defn decode [code] (apply str (code-msg (str/lower-case code) (cycle inv-key))))
+(defn decode [code] (apply str (code-msg (str/lower-case code) (cycle (inv-key)))))
 
 ;; HTML helpers
 
@@ -38,6 +39,10 @@
   (set! (.-onchange (js/document.getElementById element-id))
         (fn [event] (-> event .-target .-value handler))))
 
+(defn attach-click-handler! [element-id handler]
+  (set! (.-onclick (js/document.getElementById element-id))
+        (fn [event] (-> event .-target .-value handler))))
+
 (defn set-value! [element-id value]
   (set! (.-value (js/document.getElementById element-id)) value))
 
@@ -46,6 +51,15 @@
 
 (defn copy-to-clipboard! [value]
   (js/navigator.clipboard.writeText value))
+
+(defn parse-long [s]
+  (js/Number.parseInt s))
+
+(defn store! [name value] 
+  (js/localStorage.setItem name (prn-str value)))
+
+(defn load! [name]
+  (read-string (js/localStorage.getItem name)))
 
 ;; Handlers
 
@@ -56,11 +70,27 @@
   (set-value! "encode" (decode arg)))
 
 (defn on-change [arg]
-  (js/navigator.clipboard.writeText (get-value "decode"))
+  (copy-to-clipboard! (get-value "decode"))
   (set-value! "decode" "")
   (set-value! "encode" ""))
+
+(defn on-key [value]
+  (reset! key-vector (mapv parse-long (str/split value #" ")))
+  (store! "key" @key-vector))
+
+(defn on-hide-key [] 
+  (swap! hide-key-state not)
+  (prn hide-key-state)
+  (if @hide-key-state
+    (set-value! "key" "* * * * * * * *")
+    (set-value! "key" (str/join " " @key-vector))))
 
 (attach-input-handler! "encode" on-encode)
 (attach-input-handler! "decode" on-decode)
 (attach-change-handler! "encode" on-change)
 (attach-change-handler! "decode" on-change)
+(attach-change-handler! "key" on-key)
+(attach-click-handler! "hide-key" on-hide-key)
+(reset! key-vector (load! "key"))
+(set-value! "key" (str/join " " @key-vector))
+
